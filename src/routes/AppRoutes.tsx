dd80056975a94d8035fbf2765a57730180f081a5
Althaf Hussain <althafhussain10@gmail.com>
2026-01-10 09:43:05 +0530

    fix: Favicon and Toaster added changes [main]

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,22 +5,28 @@ import ProductDetails from "../pages/ProductDetails";
 import CartSheet from "../pages/CartSheet";
 import NotFound from "../pages/NotFound";
 import CategoryPage from "../pages/CategoryPage";
+import { Toaster } from "../components/ui/toaster";
+import { Toaster as Sonner } from "../components/ui/sonner";
 
 const AppRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<CartSheet />} />
-      <Route path="/sarees" element={<CategoryPage />} />
-      <Route path="/lehengas" element={<CategoryPage />} />
-      <Route path="/kurtis" element={<CategoryPage />} />
-      <Route path="/salwar" element={<CategoryPage />} />
-      <Route path="/new-arrivals" element={<CategoryPage />} />
-      <Route path="/sale" element={<CategoryPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Toaster />
+      <Sonner />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<CartSheet />} />
+        <Route path="/sarees" element={<CategoryPage />} />
+        <Route path="/lehengas" element={<CategoryPage />} />
+        <Route path="/kurtis" element={<CategoryPage />} />
+        <Route path="/salwar" element={<CategoryPage />} />
+        <Route path="/new-arrivals" element={<CategoryPage />} />
+        <Route path="/sale" element={<CategoryPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
